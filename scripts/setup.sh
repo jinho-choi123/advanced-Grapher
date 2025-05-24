9ac3c57ae3318b38867e50520886d1956a1e46a5
jinho-choi123 <choijinho817@gmail.com>
2025-05-24 10:56:56 +0000

    update gitignore

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
+# install corpusreader
 git clone https://gitlab.com/webnlg/corpus-reader.git corpusreader
 
+# install dataset
 wget https://github.com/jinho-choi123/advanced-Grapher/raw/refs/heads/artifact-evaluation/webnlg-dataset.zip?download= -O webnlg-dataset.zip
 unzip webnlg-dataset.zip
 
+
+# install pretrained checkpoints
 wget https://github.com/jinho-choi123/advanced-Grapher/raw/refs/heads/artifact-evaluation/output.zip?download= -O output.zip
 unzip output.zip
 
+# install model config cache
 wget https://github.com/jinho-choi123/advanced-Grapher/raw/refs/heads/artifact-evaluation/cache.zip?download= -O cache.zip
 unzip cache.zip
 
 
 # install conda dependencies
 conda env create -q -f requirements.yml -n advanced_grapher_env
-conda init
-conda activate advanced_grapher_env
