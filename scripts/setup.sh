a4822638e0605927bc26db70aa7b52317ceb14de
jinho-choi123 <choijinho817@gmail.com>
2025-05-24 04:21:07 +0000

    conda init

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 # install conda dependencies
 conda env create -q -f requirements.yml -n advanced_grapher_env
+conda init
 conda activate advanced_grapher_env
 
 git clone https://gitlab.com/webnlg/corpus-reader.git corpusreader
