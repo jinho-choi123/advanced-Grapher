46f07f4c89d7841a40dbdca425b71ff1383cd969
jinho-choi123 <choijinho817@gmail.com>
2025-05-24 11:12:41 +0000

    update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,9 @@
 # install corpusreader
 git clone https://gitlab.com/webnlg/corpus-reader.git corpusreader
 
+# install WebNLG_Text_to_triples
+git clone https://github.com/WebNLG/WebNLG-Text-to-triples.git WebNLG_Text_to_triples
+
 # install dataset
 wget https://github.com/jinho-choi123/advanced-Grapher/raw/refs/heads/artifact-evaluation/webnlg-dataset.zip?download= -O webnlg-dataset.zip
 unzip webnlg-dataset.zip
